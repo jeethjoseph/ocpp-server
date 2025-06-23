84d1d2f4ebad0fa6fa012fc1d5fe3d81c34723c6
Jeeth Joseph <jeethjoseph@gmail.com>
2025-06-23 17:25:06 +0530

    fixed cors

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://ocpp-frontend-mu.vercel.app/" ],  # Frontend origins
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://ocpp-frontend-mu.vercel.app" ],  # Frontend origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
